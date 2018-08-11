017463ee287821ce7f2b9b0466af172df8e126ad
Christopher Brown <chbphone55@gmail.com>
2018-08-10 22:20:54 -0600

    Playing an audio element stops others

--- app/components/Itunes/itunes-controller.js
+++ app/components/Itunes/itunes-controller.js
@@ -12,15 +12,22 @@ function drawSongs(songs) {
         song.preview.includes('video')
           ? ''
           : `
-    <article class='card col-md-4 col-sm-6'>
+    <article class='card col-lg-3 col-md-4 col-sm-6 mx-2 my-2'>
       <img src='${song.albumArt}' alt='album art' class='card-img-top' />
       <div class='card-body'>
-        <h3 class='card-title'>${song.title} $${song.price || 0}</h3>
-        <h4 class='card-subtitle text-muted'>Album: ${song.collection}</h4>
-        <h5 class='card-subtitle text-muted mb-2'>Artist: ${song.artist}</h5>
+        <h3 class='card-title text-truncate mb-1'>${song.title} $${song.price ||
+              0}</h3>
+        <h5 class='card-subtitle text-muted text-truncate mb-1'>Album: ${
+          song.collection
+        }</h5>
+        <h5 class='card-subtitle text-muted text-truncate mb-2'>Artist: ${
+          song.artist
+        }</h5>
       </div>
       <div class='card-footer'>
-        <audio src='${song.preview}' controls>
+        <audio class='itunes-preview' onplay='app.controllers.itunesCtrl.playAudio(event.target)' src='${
+          song.preview
+        }' controls>
           Your browser doesn't support HTML5 Audio
         </audio>
       </div>
@@ -43,6 +50,13 @@ class ItunesController {
           event.target.parentNode.classList.add('pushed-up')
         )
       );
+  }
+
+  playAudio(target) {
+    console.log('playing audio');
+    document
+      .querySelectorAll('.itunes-preview')
+      .forEach(audioElement => audioElement !== target && audioElement.pause());
   }
 }
 
