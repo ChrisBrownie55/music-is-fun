618456eb4d7a168a89abda3e7fb79ffa09334e72
Christopher Brown <chbphone55@gmail.com>
2018-08-11 21:29:05 -0600

    animate in cards

--- app/components/Itunes/itunes-controller.js
+++ app/components/Itunes/itunes-controller.js
@@ -4,57 +4,91 @@ import ItunesService from './itunes-service.js';
 
 const itunesService = new ItunesService();
 
-function drawSongs(songs) {
+function drawSongs(songs, artistName) {
   // console.log(songs);
-  document.getElementById('songs').innerHTML = songs
-    .map(
-      song =>
-        !song.preview || song.preview.includes('video')
-          ? ''
-          : `
-    <article class='card mx-2 my-2 fade' style='width: 20rem'>
-      <img src='${
-        song.albumArt
-      }' alt='album art' class='card-img-top' width='250' height='250' style='object-fit: cover'/>
-      <div class='card-body'>
-        <h3 class='card-title text-truncate w-75 mb-1'>
-          ${song.title}
-          <span class='position-absolute r-10'>$${song.price || 0}</span>
-        </h3>
-        <h5 class='card-subtitle text-muted text-truncate mb-1'>Album:
-          <span class='font-weight-normal'>${song.collection}</span>
-        </h5>
-        <h5 class='card-subtitle text-muted text-truncate mb-2'>Artist:
-          <span class='font-weight-normal'>${song.artist}</span>
-        </h5>
-      </div>
-      <div class='card-footer'>
-        <audio class='itunes-preview' onplay='app.controllers.itunesCtrl.playAudio(event.target)' src='${
-          song.preview
-        }' controls>
-          Your browser doesn't support HTML5 Audio
-        </audio>
-      </div>
-    </article>
-  `
-    )
-    .join('');
-
-  setTimeout(
-    () =>
-      document
-        .querySelectorAll('#songs > article')
-        .forEach(song => song.classList.add('show')),
-    250
-  );
+  const songsElement = document.getElementById('songs');
+  if (!songs.length) {
+    songsElement.innerHTML = `
+      <article class='alert alert-danger' id='no-results-alert' style='pointer-events: none;'>
+        <h1>No results for ${artistName}</h1>
+      </article>
+    `;
+    document
+      .getElementById('no-results-alert')
+      .animate([{ opacity: 1 }, { opacity: 0 }], {
+        duration: 500,
+        delay: 2500,
+        fill: 'forwards'
+      });
+  } else {
+    songsElement.innerHTML = songs
+      .map(
+        song =>
+          !song.preview || song.preview.includes('video')
+            ? ''
+            : `
+        <article class='card mx-2 my-2' style='width: 20rem; opacity: 0;'>
+          <img
+            src='${song.albumArt}'
+            alt='album art'
+            class='card-img-top'
+            width='250' height='250'
+            style='object-fit: cover'
+          />
+          <div class='card-body'>
+            <h3 class='card-title text-truncate w-75 mb-1'>
+              ${song.title}
+              <span class='position-absolute r-10'>$${song.price || 0}</span>
+            </h3>
+            <h5 class='card-subtitle text-muted text-truncate mb-1'>Album:
+              <span class='font-weight-normal'>${song.collection}</span>
+            </h5>
+            <h5 class='card-subtitle text-muted text-truncate mb-2'>Artist:
+              <span class='font-weight-normal'>${song.artist}</span>
+            </h5>
+          </div>
+          <div class='card-footer'>
+            <audio class='itunes-preview' onplay='app.controllers.itunesCtrl.playAudio(event.target)' src='${
+              song.preview
+            }' controls>
+              Your browser doesn't support HTML5 Audio
+            </audio>
+          </div>
+        </article>`
+      )
+      .join('');
+    const songElements = document.querySelectorAll('#songs > article');
+    songElements.forEach((song, index) => {
+      song.animate(
+        [
+          {
+            transform: 'translateY(60%)',
+            opacity: '0'
+          },
+          {
+            transform: 'translateY(0%)',
+            opacity: '1'
+          }
+        ],
+        {
+          duration: 900,
+          delay: index * 50 + 250,
+          easing: 'ease-in-out',
+          fill: 'forwards'
+        }
+      );
+    });
+  }
 }
 
 //PUBLIC
 class ItunesController {
   getMusic(event) {
     event.preventDefault();
+
     var artist = event.target.artist.value;
     const loader = document.getElementById('loader');
+
     const formInputWrapper = event.target.children[1];
     const hideInputAnimation = formInputWrapper.animate(
       [
@@ -68,11 +102,15 @@ class ItunesController {
       }
     );
     hideInputAnimation.onfinish = () => loader.classList.add('active');
+
     itunesService.getMusicByArtist(artist).then(songs =>
       setTimeout(() => {
-        setTimeout(() => drawSongs(songs), 250);
+        setTimeout(() => drawSongs(songs, artist), 250);
+
         event.target.parentNode.classList.add('pushed-up');
+
         loader.classList.remove('active');
+
         formInputWrapper.animate(
           [
             { width: '0%', flexWrap: 'nowrap', overflow: 'hidden' },
